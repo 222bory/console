075de1cf41693a1b711c945be9c41beb15808644
SO <SO@SO-PC>
2018-01-25 13:18:20 +0900

    테이블 변경으로 관리자 계정 생성 소스 변경

--- src/main/java/com/sicc/admin/demo/model/Member.java
+++ src/main/java/com/sicc/admin/demo/model/Member.java
@@ -12,6 +12,36 @@ public class Member {
 	private String password;
 	private Date adDate;
 	private Date udtDate;
+	private String crtId;
+	private String crtIp;
+	private String udtId;
+	private String udtIp;
+	
+	
+	public String getCrtId() {
+		return crtId;
+	}
+	public void setCrtId(String crtId) {
+		this.crtId = crtId;
+	}
+	public String getCrtIp() {
+		return crtIp;
+	}
+	public void setCrtIp(String crtIp) {
+		this.crtIp = crtIp;
+	}
+	public String getUdtId() {
+		return udtId;
+	}
+	public void setUdtId(String udtId) {
+		this.udtId = udtId;
+	}
+	public String getUdtIp() {
+		return udtIp;
+	}
+	public void setUdtIp(String udtIp) {
+		this.udtIp = udtIp;
+	}
 	public String getAdminId() {
 		return adminId;
 	}
