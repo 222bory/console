bd20ea718b858ccce3837126a813ddf050091cc8
ransang <ransang@ransang-PC>
2018-01-30 17:50:24 +0900

    new

---
+++ src/main/java/com/sicc/console/model/ConCustCont.java
@@ -0,0 +1,135 @@
+package com.sicc.console.model;
+
+import java.util.Date;
+
+public class ConCustCont {
+	
+	private String tenantId;
+	private String custId;
+	private String contNm;
+	private Date validStartDt;
+	private Date validEndDt;
+	private String contStatCd;
+	private String networkFgCd;
+	private String passwordLodCd;
+	private String passwordMinLen;
+	private String passwordRnwlCyclCd;
+	private String passwordUseLmtYn;
+	private String passwordPoseYn;
+	private String crtId;
+	private String crtIp;
+	private Date adDate;
+	private String udtId;
+	private String udtIp;
+	private Date udtDate;
+	
+	public String getTenantId() {
+		return tenantId;
+	}
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
+	public String getCustId() {
+		return custId;
+	}
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public String getContNm() {
+		return contNm;
+	}
+	public void setContNm(String contNm) {
+		this.contNm = contNm;
+	}
+	public Date getValidStartDt() {
+		return validStartDt;
+	}
+	public void setValidStartDt(Date validStartDt) {
+		this.validStartDt = validStartDt;
+	}
+	public Date getValidEndDt() {
+		return validEndDt;
+	}
+	public void setValidEndDt(Date validEndDt) {
+		this.validEndDt = validEndDt;
+	}
+	public String getContStatCd() {
+		return contStatCd;
+	}
+	public void setContStatCd(String contStatCd) {
+		this.contStatCd = contStatCd;
+	}
+	public String getNetworkFgCd() {
+		return networkFgCd;
+	}
+	public void setNetworkFgCd(String networkFgCd) {
+		this.networkFgCd = networkFgCd;
+	}
+	public String getPasswordLodCd() {
+		return passwordLodCd;
+	}
+	public void setPasswordLodCd(String passwordLodCd) {
+		this.passwordLodCd = passwordLodCd;
+	}
+	public String getPasswordMinLen() {
+		return passwordMinLen;
+	}
+	public void setPasswordMinLen(String passwordMinLen) {
+		this.passwordMinLen = passwordMinLen;
+	}
+	public String getPasswordRnwlCyclCd() {
+		return passwordRnwlCyclCd;
+	}
+	public void setPasswordRnwlCyclCd(String passwordRnwlCyclCd) {
+		this.passwordRnwlCyclCd = passwordRnwlCyclCd;
+	}
+	public String getPasswordUseLmtYn() {
+		return passwordUseLmtYn;
+	}
+	public void setPasswordUseLmtYn(String passwordUseLmtYn) {
+		this.passwordUseLmtYn = passwordUseLmtYn;
+	}
+	public String getPasswordPoseYn() {
+		return passwordPoseYn;
+	}
+	public void setPasswordPoseYn(String passwordPoseYn) {
+		this.passwordPoseYn = passwordPoseYn;
+	}
+	public String getCrtId() {
+		return crtId;
+	}
+	public void setCrtId(String crtId) {
+		this.crtId = crtId;
+	}
+	public String getCrtIp() {
+		return crtIp;
+	}
+	public void setCrtIp(String crtIp) {
+		this.crtIp = crtIp;
+	}
+	public Date getAdDate() {
+		return adDate;
+	}
+	public void setAdDate(Date adDate) {
+		this.adDate = adDate;
+	}
+	public String getUdtId() {
+		return udtId;
+	}
+	public void setUdtId(String udtId) {
+		this.udtId = udtId;
+	}
+	public String getUdtIp() {
+		return udtIp;
+	}
+	public void setUdtIp(String udtIp) {
+		this.udtIp = udtIp;
+	}
+	public Date getUdtDate() {
+		return udtDate;
+	}
+	public void setUdtDate(Date udtDate) {
+		this.udtDate = udtDate;
+	}
+	
+}
